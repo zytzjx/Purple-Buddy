05ab9c7dd949f0f2cb0e734a9ded04263844abaf
Adderally <56490828+Adderally@users.noreply.github.com>
2021-06-21 22:04:19 -0500

    Add files via upload

--- iMobileDeviceReturnCodes.cs
+++ iMobileDeviceReturnCodes.cs
@@ -13,5 +13,10 @@ namespace PurpleBuddy
         /// https://github.com/libimobiledevice-win32/libideviceactivation/blob/msvc/master/include/libideviceactivation.h
         /// </summary>
         public int ACTIVATION_IDEVICE_ACTIVATION_E_SUCCESS = 0;
+
+        /// <summary>
+        /// https://github.com/libimobiledevice/libimobiledevice/blob/master/include/libimobiledevice/diagnostics_relay.h
+        /// </summary>
+        public int DIAGNOSTICS_RELAY_E_SUCCESS = 0;
     }
 }
