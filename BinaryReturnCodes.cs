d6e39568ec2c55c5b6dc948a55e7b34499812c34
Adderally <56490828+Adderally@users.noreply.github.com>
2021-06-27 14:13:09 -0500

    Add files via upload

---
+++ BinaryReturnCodes.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PurpleBuddy
+{
+    public class BinaryReturnCodes
+    {
+        /// <summary>
+        /// https://github.com/libimobiledevice/libimobiledevice/blob/master/include/libimobiledevice/diagnostics_relay.h
+        /// </summary>
+        public enum DiagnosticsRelay
+        {
+            Success = 0,
+            ArgumentInvalid = -1,
+            PlistError = -2,
+            MuxError = -3,
+            UnknownError = -256
+        }
+
+        /// <summary>
+        /// https://github.com/libimobiledevice-win32/libideviceactivation/blob/msvc/master/include/libideviceactivation.h
+        /// </summary>
+        public enum DeviceActivation
+        {
+            Success = 0,
+            IncompleteInfo = -1,
+        }
+    }
+}
