629525d122dd2ade7fd600f7d072a74989e16545
Adderally <56490828+Adderally@users.noreply.github.com>
2021-06-27 14:15:33 -0500

    Add files via upload

---
+++ Primitive/Commands/Essentials.cs
@@ -0,0 +1,24 @@
+﻿using System.IO;
+
+
+namespace PurpleBuddy.Primitive.Commands
+{
+    public class Essentials
+    {
+        /// <summary>
+        /// Directory where iMobile binaries should be located.
+        /// File names need to be appended!
+        /// </summary>
+        public string BinaryPath { get; } = Directory.GetCurrentDirectory() + "\\" + "win-x64";
+
+        /// <summary>
+        /// idevicediagnostics true file name.
+        /// </summary>
+        public string DeviceDiagnostics { get; } = "idevicediagnostics.exe";
+
+        /// <summary>
+        /// ideviceactivation true file name.
+        /// </summary>
+        public string DeviceActivation { get; } = "ideviceactivation.exe";
+    }
+}
---
+++ Primitive/Commands/iMobileNormalCommands.cs
@@ -0,0 +1,143 @@
+﻿using System.Diagnostics;
+using iMobileDevice;
+using iMobileDevice.iDevice;
+using iMobileDevice.Lockdown;
+using iMobileDevice.Recovery;
+using PurpleBuddy;
+using PurpleBuddy.Primitive.Commands;
+
+namespace Prizrak
+{
+    /// <summary>
+    /// Overall general commands accross iOS devices.
+    /// </summary>
+    public class iMobileNormalCommands
+    {      
+        
+        Essentials Helper = new Essentials();
+             
+
+        /// <summary>
+        /// Boots all iOS devices based on user option.
+        /// </summary>
+        /// <param name="udid">the device UDID as string.</param>
+        /// <param name="bootoption">the boot option; 1 = shutdown devices, 2 = reboot / restart devices.</param>
+        public void mass_boot_iosdevice(string udid, int bootoption)
+        {
+            
+            switch (bootoption)
+            {
+                case 1:
+                    using (Process process = new Process())
+                    {
+                        process.StartInfo.FileName        = Helper.BinaryPath + "\\" + $"[{Helper.DeviceDiagnostics}]";
+                        process.StartInfo.Arguments       = "-u " + udid + " shutdown";
+                        process.StartInfo.CreateNoWindow  = true;
+                        process.StartInfo.UseShellExecute = false;
+                        process.Start();                  
+                    }
+                    break;
+
+                case 2:
+                    using (Process process = new Process())
+                    {
+                        process.StartInfo.FileName        = Helper.BinaryPath + "\\" + $"[{Helper.DeviceDiagnostics}]";
+                        process.StartInfo.Arguments       = "-u " + udid + " restart";
+                        process.StartInfo.CreateNoWindow  = true;
+                        process.StartInfo.UseShellExecute = false;
+                        process.Start();
+                    }
+                    break;
+
+                default:
+                    return;
+            }            
+        }
+
+
+        /// <summary>
+        /// Activates / Deactivates all iOS devices based on user option.
+        /// </summary>
+        /// <param name="udid">the device UDID as string.</param>
+        /// <param name="activation_option">Activation options; 1 = activate devices, 2 = deactivate devices.</param>
+        public void mass_activation_set_device_activation_state(in string udid, int activation_option)
+        {
+
+            int r_code = 0;
+            bool ActiviationMethodIsSuccess;
+
+
+            switch (activation_option)
+            {
+                case 1:
+                    using (Process process = new Process())
+                    {
+                        process.StartInfo.FileName        = Helper.BinaryPath + "\\" + $"[{Helper.DeviceActivation}]";
+                        process.StartInfo.Arguments       = "-u " + udid + " activate";
+                        process.StartInfo.CreateNoWindow  = true;
+                        process.StartInfo.UseShellExecute = false;                   
+                        process.Start();
+                        process.WaitForExit();
+
+                        r_code = process.ExitCode;
+                        ActiviationMethodIsSuccess = (r_code == (int)BinaryReturnCodes.DeviceActivation.Success) ? true : false;
+                    }
+                    break;
+
+                case 2:
+                    using (Process process = new Process())
+                    {
+                        process.StartInfo.FileName        = Helper.BinaryPath + "\\" + $"[{Helper.DeviceActivation}]";
+                        process.StartInfo.Arguments       = "-u " + udid + " deactivate";
+                        process.StartInfo.CreateNoWindow  = true;
+                        process.StartInfo.UseShellExecute = false;
+                        process.Start();
+                        process.WaitForExit();
+
+                        r_code = process.ExitCode;
+                        ActiviationMethodIsSuccess = (r_code == (int)BinaryReturnCodes.DeviceActivation.Success) ? true : false;
+                    }
+                    break;
+
+                default:
+                    return;
+            }                      
+        }
+
+
+        /// <summary>
+        /// Put all iOS devices in recovery mode.
+        /// </summary>
+        /// <param name="idevice">The device handle.</param>
+        public void mass_recovery_enterrecoverymode(iDeviceHandle idevice)
+        {
+            var lockdown = LibiMobileDevice.Instance.Lockdown;
+            LockdownClientHandle lockdowndevice;
+            
+            lockdown.lockdownd_client_new_with_handshake(idevice, out lockdowndevice, "Ghost");            
+            lockdown.lockdownd_enter_recovery(lockdowndevice);           
+        }
+
+
+        /// <summary>
+        /// Exit recovery loop.
+        /// </summary>
+        
+        public void recovery_exitrecoverymode()
+        {
+            var recoverydevice = LibiMobileDevice.Instance.Recovery;
+
+            RecoveryClientHandle recoveryClientHandle = null;
+            RecoveryDeviceHandle recoveryDeviceHandle = null;
+            
+            ulong ecid = 0;
+
+            recoverydevice.irecv_devices_get_all();
+            recoverydevice.irecv_open_with_ecid(out recoveryClientHandle, ecid);
+            recoverydevice.irecv_devices_get_device_by_client(recoveryClientHandle, out recoveryDeviceHandle);
+            recoverydevice.irecv_setenv(recoveryClientHandle, "auto-boot", "true");
+            recoverydevice.irecv_saveenv(recoveryClientHandle);
+            recoverydevice.irecv_reboot(recoveryClientHandle);
+        }
+    }
+}
